a2af96ca34f73869193931b803622889d6bab85a
ikepu-tp <postmaster@licsss.jp>
2023-12-13 07:05:26 +0900

    add TFA

--- src/app/Http/Middleware/TFAuthMiddleware.php
+++ src/app/Http/Middleware/TFAuthMiddleware.php
@@ -3,12 +3,21 @@
 namespace ikepu_tp\SecureAuth\app\Http\Middleware;
 
 use Closure;
+use ikepu_tp\SecureAuth\app\Models\TFA;
 use Illuminate\Http\Request;
 use Illuminate\Validation\UnauthorizedException;
 use Symfony\Component\HttpFoundation\Response;
 
 class TFAuthMiddleware
 {
+    /** @var TFA */
+    public $tfa;
+
+    function __construct()
+    {
+        $this->tfa = new TFA();
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -17,6 +26,24 @@ class TFAuthMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if (!$request->user(config("secure-auth.guard"))) throw new UnauthorizedException();
+
+        if (empty($request->session()->get("__tfa"))) {
+            return $this->setTFA($request);
+        } else {
+            if (!$this->tfa->auth) return $this->redirectToAuth();
+        }
         return $next($request);
+    }
+
+    private function setTFA(Request $request): Response
+    {
+        $this->tfa->generateTFA();
+        $this->tfa->setTFA();
+        return $this->redirectToAuth();
+    }
+
+    private function redirectToAuth(): Response
+    {
+        return response()->redirectTo("");
     }
 }
---
+++ src/app/Models/TFA.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace ikepu_tp\SecureAuth\app\Models;
+
+use DateTime;
+
+class TFA
+{
+    public string $value = "";
+    public bool $auth = false;
+    public string $expired_at = "";
+    public bool $valid = false;
+
+    function __construct(array $tfa = [])
+    {
+        $tfa = array_merge(
+            $this->getTFA(),
+            $tfa
+        );
+        if (isset($tfa["value"]) && is_string($tfa["value"])) $this->value = $tfa["value"];
+        if (isset($tfa["auth"]) && is_bool($tfa["auth"])) $this->auth = $tfa["auth"];
+        if (isset($tfa["expired_at"]) && is_string($tfa["expired_at"])) $this->expired_at = $tfa["expired_at"];
+        $this->isValid();
+    }
+
+    /**
+     * 値の有無
+     *
+     * @return boolean
+     */
+    public function isValid(): bool
+    {
+        $this->valid =
+            !empty($this->value) && !empty($this->expired_at);
+        return $this->valid;
+    }
+
+    public function toArray(): array|null
+    {
+        if ($this->isValid())
+            return [
+                "value" => $this->value,
+                "auth" => $this->auth,
+                "expired_at" => $this->expired_at,
+            ];
+        return null;
+    }
+
+    /**
+     * TFA作成
+     *
+     * @return void
+     */
+    public function generateTFA(): void
+    {
+        if ($this->isValid()) return;
+        $this->value = (string)str_pad(rand(0, 999999), 6, 0, STR_PAD_LEFT);
+        $this->auth = false;
+        $this->expired_at = now()->addMinutes(config("secure-auth.expires_minutes", 10));
+        $this->valid = true;
+    }
+
+    public function revokeTFA(): void
+    {
+        $this->value = "";
+        $this->auth = false;
+        $this->expired_at = "";
+        $this->valid = false;
+        $this->setTFA(null);
+    }
+
+    /**
+     * TFA設定
+     *
+     * @return void
+     */
+    public function setTFA(array|null $value = null): void
+    {
+        if (is_null($value) && $this->isValid()) {
+            $this->generateTFA();
+            $value = $this->toArray();
+        }
+        session()->push("__tfa", $value);
+    }
+
+    public function getTFA(): array
+    {
+        $tfa = session("__tfa", []);
+        return $tfa ? $tfa[0] : [];
+    }
+
+    /**
+     * TFA認証
+     *
+     * @param string $tfa_token
+     * @return boolean
+     */
+    public static function authorizeTFA(string $tfa_token): bool
+    {
+        $tfa = new static();
+        $tfa->auth = (
+            !$tfa->isValid() ||
+            $tfa->isExpired() ||
+            !$tfa->checkToken($tfa_token)
+        );
+        $tfa->setTFA($tfa->toArray());
+        return $tfa->auth;
+    }
+
+    /**
+     * 有効期限切れ
+     *
+     * @return boolean
+     */
+    public function isExpired(): bool
+    {
+        $now = now()->getTimestamp();
+        $expired_at = (new DateTime($this->expired_at))->getTimestamp();
+        return $now > $expired_at;
+    }
+
+    /**
+     * トークンの一致確認
+     *
+     * @param string $tfa_token
+     * @return boolean
+     */
+    public function checkToken(string $tfa_token): bool
+    {
+        return $this->value === $tfa_token;
+    }
+}
