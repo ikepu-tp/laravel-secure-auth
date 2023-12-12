1486db50eb2291386c26604f33bd68f6f539fed6
ikepu-tp <postmaster@licsss.jp>
2023-12-12 11:45:27 +0900

    first init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Laravel Secure Auth
+
+This is a package for secure authentication on Laravel.
+
+## How to use
+
+## Contributing
+
+We welcome contributions to the project! You can get involved through the following ways:
+
+Issue: Use for bug reports, feature suggestions, and more.
+Pull Requests: We encourage code contributions for new features and bug fixes.
+
+## License
+
+See [LICENSE](./LICENSE).
---
+++ src/SecureAuthServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace ikepu_tp\SecureAuth;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
+
+class SecureAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config/secure-auth.php', 'secure-auth');
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->registerPublishing();
+        $this->defineRoutes();
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadViewsFrom(__DIR__ . "/resources/views", "SecureAuth");
+        Paginator::useBootstrap();
+        Blade::componentNamespace("ikepu_tp\\resources\\views\\components", "SecureAuth");
+    }
+
+    /**
+     * Register the package's publishable resources.
+     */
+    private function registerPublishing()
+    {
+        if (!$this->app->runningInConsole()) return;
+
+        $this->publishes([
+            __DIR__ . '/config/secure-auth.php' => base_path('config/secure-auth.php'),
+        ], 'SecureAuth-config');
+
+
+        $this->publishMigration();
+        $this->publishView();
+        $this->publishAsset();
+    }
+
+    private function publishMigration(): void
+    {
+        $migrations = [];
+        foreach ($migrations as $migration) {
+            $this->publishes([
+                __DIR__ . "/database/migrations/{$migration}" => database_path(
+                    "migrations/{$migration}"
+                ),
+            ], 'migrations');
+        }
+    }
+
+    private function publishView(): void
+    {
+        $this->publishes([
+            __DIR__ . '/resources/views' => resource_path('views/vendor/SecureAuth'),
+        ], 'SecureAuth-views');
+    }
+
+    private function publishAsset(): void
+    {
+        $this->publishes([], 'SecureAuth-assets');
+    }
+
+    /**
+     * Define the routes.
+     *
+     * @return void
+     */
+    protected function defineRoutes()
+    {
+        $this->loadRoutesFrom(__DIR__ . "/routes/web.php");
+    }
+}
